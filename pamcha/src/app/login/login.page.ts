032f382abb287bffd1f6e6e8e37e5456e026dee8
Francisca Ramirez <107446606+franc-ramireza@users.noreply.github.com>
2023-09-23 14:20:31 +0000

    avances

---
+++ pamcha/src/app/login/login.page.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+}from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  formulariologin: FormGroup
+
+  constructor(public fb: FormBuilder) { 
+    this.formulariologin = this.fb.group({
+      'nombre': new FormControl("",Validators.required),
+      'password': new FormControl("",Validators.required)
+    })
+  }
+
+  ngOnInit() {
+  }
+
+}
