0de08273fac033a5682cef2261d65375b15a73bc
Francisca Ramirez <107446606+franc-ramireza@users.noreply.github.com>
2023-10-09 03:48:08 +0000

    avances home

--- pamcha/src/app/home/home.page.ts
+++ pamcha/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nombre: string = "";
+  apellido: string = "";
+  educacion: string = "";
+  fechaNacimiento: string = "";
 
-  constructor() {}
+  constructor(private toastController: ToastController) {}
 
+  limpiar() {
+    // Limpia los campos
+    this.nombre = "";
+    this.apellido = "";
+    this.educacion = "";
+    this.fechaNacimiento = "";
+  }
+
+  async mostrar() {
+    // Verifica si se ingresó el nombre y el apellido
+    if (!this.nombre || !this.apellido) {
+      const toast = await this.toastController.create({
+        message: "Por favor, ingresa el nombre y el apellido",
+        duration: 2000,
+        position: "bottom",
+      });
+      await toast.present();
+    } else {
+      // Muestra un mensaje emergente con el nombre y apellido
+      const toast = await this.toastController.create({
+        message: `Nombre: ${this.nombre}, Apellido: ${this.apellido}`,
+        duration: 2000,
+        position: "bottom",
+      });
+      await toast.present();
+    }
+  }
 }
